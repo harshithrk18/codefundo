c1faf1582cbdb96b363ae859107bd4748a105a3b
harshithrk18 <39452270+harshithrk18@users.noreply.github.com>
2018-10-27 08:14:57 +0530

    Update urls.py

--- codefundo/urls.py
+++ codefundo/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+# from django.urls import path, include
+from django.conf.urls import url,include
 from merlin import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
-    path('merlin/', include('merlin.urls')),
-    path('map/', views.map, name='map'),
-    path('logout/', views.user_logout, name='logout')
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.index,name='index'),
+    url(r'^merlin/', include('merlin.urls')),
+    url(r'^map/', views.map, name='map'),
+    url(r'^logout/', views.user_logout, name='logout')
 
 ]
